26b3420c26a306aad232bba07d16ebcfbd41e8a6
AklavyaKUMAR <74763533+AklavyaKUMAR@users.noreply.github.com>
2021-03-03 14:30:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+
+var  ground , paper1 ;
+var wallSprite , wall2Sprite , wall3Sprite ;
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+
+ 
+function setup() {
+  createCanvas(1200, 600);
+  
+  engine = Engine.create();
+	world = engine.world;
+
+
+  paper = new Papper(100,470,20);
+  
+	ground = new Ground(600 , 500 ,1200 , 20 );
+  
+  wallSprite=createSprite(900 , 485 , 150,10);
+	wallSprite.shapeColor=color("red")
+
+	wall2Sprite=createSprite(820 , 445,10,90);
+	wall2Sprite.shapeColor=color("red")
+
+	wall3Sprite=createSprite(980 , 445 , 10 , 90);
+	wall3Sprite.shapeColor=color("red")
+
+
+   
+    Engine.run(engine);
+  
+}
+
+
+function draw() {
+
+  background(0);
+  paper.display();
+  ground.display();
+ 
+  drawSprites();
+}
+
+function keyPressed() {
+if (keyCode === UP_ARROW) {
+  Matter.Body.applyForce( paper.body,paper.body.position,{x:85,y:-85});
+}
+
+}
+
